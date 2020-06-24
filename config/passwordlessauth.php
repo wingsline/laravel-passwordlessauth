14a810a8950e6d2236690b67953c051be9b65a71
Arpad Olasz <wingsline@gmail.com>
2020-06-24 11:41:07 -0500

    Add remember option when login.

--- config/passwordlessauth.php
+++ config/passwordlessauth.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
+
     'redirectTo' => 'home',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Remember login when using passwordless auth
+    |--------------------------------------------------------------------------
+    |
+    | Set to true in order to remember the signed in user.
+    |
+    */
+    'remember' => env('PASSWORDLESS_REMEMBER', true),
 ];
